1f6743d1266cc82f2458d33b61fb7fa8e7e327ad
brayan <brayan3288@gmail.com>
2020-03-22 14:30:58 -0500

    Header ok

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <px-header msg="Welcome to Your Vue.js App"/>
+    <main class="ui container">
+      <px-lista-monedas/>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PxHeader from './components/globals/PxHeader.vue'
+import PxListaMonedas from './components/PxListaMonedas'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PxHeader,
+    PxListaMonedas
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/globals/PxHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <nav class="ui block header olive">
+      <px-icon />
+      <span>Vueirtual-Coin</span>
+    </nav>
+  </header>
+</template>
+
+<script>
+import PxIcon from "./PxIcon";
+export default {
+  name: "PxHeader",
+  components: {
+    PxIcon
+  }
+};
+</script>
+
+<style scoped>
+span {
+  font-size: 24px;
+  margin-top: 0px;
+  margin-left: 10px;
+}
+nav {
+   background-color: #869D05;
+}
+</style>
